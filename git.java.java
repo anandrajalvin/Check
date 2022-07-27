981f04506c21f738b2dffcf608e6c88cc0030a48
anandrajalvin <109920397+anandrajalvin@users.noreply.github.com>
2022-07-28 03:10:39 +0530

    Add files via upload

---
+++ git.java.java
@@ -0,0 +1,12 @@
+public class git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Anand");
+		System.out.println("Hello Alvin");
+                System.out.println("Hello git");
+
+	}
+
+}
